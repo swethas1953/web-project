2624cce117b168f5a0fb900f3aa0531eb8647226
swethas1953 <swethaprabha444@gmail.com>
2024-11-20 12:55:01 +0530

    add project

--- src/test/java/com/sg/methods/UserModuleMethods.java
+++
@@ -1,7 +0,0 @@
-package com.sg.methods;
-
-import com.sg.driver.DriverScript;
-
-public class UserModuleMethods extends DriverScript{
-
-}
--- src/test/java/com/sg/testScripts/TestModuleScripts.java
+++
@@ -1,7 +0,0 @@
-package com.sg.testScripts;
-
-import com.sg.driver.DriverScript;
-
-public class TestModuleScripts extends DriverScript{
-
-}
